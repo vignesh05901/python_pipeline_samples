70673c9351aae36d843dfe90a96e94ba97d5fd4c
vignesh0590 <vignesh0590@gmail.com>
2023-01-01 12:58:07 +0530

    added python yaml files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_input = request.form['text']
+        return render_template('echo.html', user_input=user_input)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
